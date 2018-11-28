c5886bbe40000b0e2d77de66fd33e5dd525b6e7a
wumengyu <18375330004@163.com>
2018-11-28 10:35:52 +0800

    项目工具类的添加

---
+++ src/main/java/com/lawschool/util/PageUtils.java
@@ -0,0 +1,70 @@
+package com.lawschool.util;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PageUtils implements Serializable {
+	private static final long serialVersionUID = 1L;
+	//总记录数
+	private long totalCount;
+	//每页记录数
+	private long pageSize;
+	//总页数
+	private long totalPage;
+	//当前页数
+	private int currPage;
+	//列表数据
+	private List<?> list;
+	
+	public PageUtils(List<?> list, long totalCount, long pageSize, int currPage) {
+		this.list = list;
+		this.totalCount = totalCount;
+		this.pageSize = pageSize;
+		this.currPage = currPage;
+		this.totalPage = (int)Math.ceil((double)totalCount/pageSize);
+	}
+
+
+
+	public long getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(long totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public long getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(long pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public long getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public int getCurrPage() {
+		return currPage;
+	}
+
+	public void setCurrPage(int currPage) {
+		this.currPage = currPage;
+	}
+
+	public List<?> getList() {
+		return list;
+	}
+
+	public void setList(List<?> list) {
+		this.list = list;
+	}
+	
+}
