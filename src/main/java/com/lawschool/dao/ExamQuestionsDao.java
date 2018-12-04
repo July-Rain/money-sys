dbf5af3fea8ff6627413b4bde3207f7c236f2408
wangshuaiqiGit <969282037@qq.com>
2018-12-04 15:02:39 +0800

    试卷生成

---
+++ src/main/java/com/lawschool/dao/ExamQuestionsDao.java
@@ -0,0 +1,8 @@
+package com.lawschool.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.lawschool.beans.ExamQuestions;
+
+public interface ExamQuestionsDao extends BaseMapper<ExamQuestions>{
+
+}
---
+++ src/main/java/com/lawschool/service/ExamQuestionsService.java
@@ -0,0 +1,8 @@
+package com.lawschool.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.lawschool.beans.ExamQuestions;
+
+public interface ExamQuestionsService extends IService<ExamQuestions> {
+
+}
