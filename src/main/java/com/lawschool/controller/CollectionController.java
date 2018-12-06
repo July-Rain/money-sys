1247c3ad6ecfa25917a6af5fac1ee6b739394094
zhujunwen <931078457@qq.com>
2018-12-06 09:42:48 +0800

    change

--- src/main/java/com/lawschool/controller/CollectionController.java
+++ src/main/java/com/lawschool/controller/CollectionController.java
@@ -1,14 +1,22 @@
 package com.lawschool.controller;
 
+import com.lawschool.beans.Answer;
 import com.lawschool.beans.Collection;
+import com.lawschool.beans.TestQuestions;
+import com.lawschool.beans.User;
+import com.lawschool.service.AnswerService;
 import com.lawschool.service.CollectionService;
 import com.lawschool.util.Constant;
+import com.lawschool.util.PageUtils;
 import com.lawschool.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 import static com.lawschool.util.Constant.SUCCESS;
 
@@ -22,17 +30,110 @@ public class CollectionController extends AbstractController{
     @Autowired
     CollectionService collectionService;
 
+    @Autowired
+    AnswerService answerService;
+
+    /**
+     * @Author zjw
+     * @Description 删除收藏
+     * @Date 9:40 2018-12-6
+     * @Param [collection]
+     * @return com.lawschool.util.Result
+    **/
     @RequestMapping("/delColl")
     public Result delCollection(@RequestBody Collection collection){
         int rest = collectionService.delCollection(collection, getUser());
         return rest==SUCCESS?Result.ok():Result.error("取消收藏失败");
     }
 
+    /**
+     * @Author zjw
+     * @Description 添加收藏
+     * @Date 9:40 2018-12-6
+     * @Param [collection]
+     * @return com.lawschool.util.Result
+    **/
     @RequestMapping("/addColl")
     public Result addCollection(@RequestBody Collection collection){
         int rest = collectionService.addCollection(collection,getUser());
         return rest== SUCCESS ?Result.ok():Result.error("收藏失败");
     }
 
+
+
+    /**
+     * @Author zjw
+     * @Description 我收藏的题目
+     * @Date 9:41 2018-12-6
+     * @Param [params]
+     * @return com.lawschool.util.Result
+    **/
+    @RequestMapping("/testQuestion")
+    public Result listMyCollection(@RequestParam Map<String,Object> params){
+        params.put("userId",getUser().getId());
+        PageUtils pageUtils = collectionService.listMyCollection(params);
+        return Result.ok().put("result",pageUtils);
+    }
+
+    /**
+     * @Author zjw
+     * @Description 一键组卷（收藏的题目）
+     * @Date 9:41 2018-12-6
+     * @Param [params]
+     * @return com.lawschool.util.Result
+    **/
+    @RequestMapping("/randomQuestColl")
+    public Result randomQuestColl(@RequestParam Map<String,Object> params){
+
+//        Result result=testQuestionService.randomQuestColl(params,getUser());
+
+        //测试代码
+        User user=new User();
+        user.setId("1");
+        Result result=collectionService.randomQuestColl(params,user);
+        return result;
+    }
+
+
+    /**
+     * @Author zjw
+     * @Description 获取我做错的题目
+     * @Date 9:41 2018-12-6
+     * @Param [params]
+     * @return com.lawschool.util.Result
+    **/
+    @RequestMapping("/errorTestQuestion")
+    public Result listMyErrorQuestion(@RequestParam Map<String,Object> params){
+        //params.put("userId",getUser().getId());
+        params.put("userId",1);
+        PageUtils pageUtils = collectionService.listMyErrorQuestion(params);
+        return Result.ok().put("result",pageUtils);
+    }
+
+
+    /**
+     * @Author zjw
+     * @Description 组卷（错题）
+     * @Date 9:42 2018-12-6
+     * @Param [params]
+     * @return com.lawschool.util.Result
+    **/
+    @RequestMapping("/randomErrorColl")
+    public Result randomErrorColl(@RequestParam Map<String,Object> params){
+        //params.put("userId",getUser().getId());
+        //测试代码
+        User user=new User();
+        user.setUserId("1");
+        Result result=collectionService.randomErrorColl(params,user);
+        return Result.ok().put("result",result);
+    }
+
+    //获取试题详情z
+    @RequestMapping("/getTestQuestion")
+    public Result getTestQuestion(@RequestBody TestQuestions testQuestions){
+        TestQuestions info = collectionService.getTestQuestions(testQuestions);
+        List<Answer> answerByQid = answerService.getAnswerByQid(testQuestions.getId());
+        return Result.ok().put("qustion",info).put("answer",answerByQid);
+    }
 
 }
