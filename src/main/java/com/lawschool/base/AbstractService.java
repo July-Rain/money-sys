b143713b8c8e14f66c2987325961379110f95ec1
wangtongye <xiaoer.wang@qq.com>
2018-12-06 11:29:39 +0800

    基类修改

--- src/main/java/com/lawschool/base/AbstractService.java
+++ src/main/java/com/lawschool/base/AbstractService.java
@@ -1,6 +1,6 @@
 package com.lawschool.base;
 
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.service.IService;
 
 public interface AbstractService<T> extends IService<T> {
@@ -12,5 +12,5 @@ public interface AbstractService<T> extends IService<T> {
      * @param entity
      * @return
      */
-    Page<T> findPage(Page<T> page, EntityWrapper<T> entity);
+    Page<T> findPage(Page<T> page, Wrapper<T> entity);
 }
