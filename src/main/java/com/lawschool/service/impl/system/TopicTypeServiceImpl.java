6dcc1955bfd2744c28f74731aa5f77af0f28643b
xuxiang <2434844600@qq.com>
2018-12-12 00:20:11 +0800

    主题

--- src/main/java/com/lawschool/service/impl/system/TopicTypeServiceImpl.java
+++ src/main/java/com/lawschool/service/impl/system/TopicTypeServiceImpl.java
@@ -3,8 +3,12 @@ package com.lawschool.service.impl.system;
 import com.lawschool.base.AbstractServiceImpl;
 import com.lawschool.beans.system.TopicTypeEntity;
 import com.lawschool.dao.system.TopicTypeDao;
+import com.lawschool.form.CommonForm;
 import com.lawschool.service.system.TopicTypeService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @version V1.0
@@ -23,5 +27,13 @@ public class TopicTypeServiceImpl extends AbstractServiceImpl<TopicTypeDao, Topi
     public boolean updateDelFlag(String id){
 
         return dao.updateDelFlag(id);
+    }
+
+    public List<CommonForm> findAll(List<String> list){
+        List<CommonForm> resultList = new ArrayList<>();
+
+        resultList = dao.findAll(list);
+
+        return resultList;
     }
 }
