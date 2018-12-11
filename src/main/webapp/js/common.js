790896cb3414ca268105689241635581f73f962f
LiKunyu <122365702@qq.com>
2018-12-11 09:25:56 +0800

    获取url中的参数

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -25,3 +25,10 @@ function checkFile(file) {
     }
     return extension1 || extension2 || extension3 || extension4 || extension5 || extension6 && isLt2M
 }
+
+// 获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
