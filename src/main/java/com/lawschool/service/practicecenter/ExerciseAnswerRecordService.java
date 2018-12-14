b6299340602cdf4436bef516c62d1949e5d36c9f
xuxiang <2434844600@qq.com>
2018-12-14 14:54:52 +0800

    随机练习

---
+++ src/main/java/com/lawschool/service/practicecenter/ExerciseAnswerRecordService.java
@@ -0,0 +1,19 @@
+package com.lawschool.service.practicecenter;
+
+import com.lawschool.base.AbstractService;
+import com.lawschool.beans.practicecenter.ExerciseAnswerRecordEntity;
+import com.lawschool.form.ThemeAnswerForm;
+
+import java.util.List;
+
+/**
+ * @version V1.0
+ * @Description: 随机练习Service
+ * @author: 中石电子科技 徐祥
+ * @date: 2018-12-14 09:33
+ */
+public interface ExerciseAnswerRecordService extends AbstractService<ExerciseAnswerRecordEntity> {
+    void saveBatch(List<ExerciseAnswerRecordEntity> list);
+
+    List<ThemeAnswerForm> analysisAnswer(String randomId);
+}
