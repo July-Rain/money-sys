1f7dedf61b0d9829472a4a74a7ae50529dc02e52
wumengyu <18375330004@163.com>
2018-12-18 14:31:54 +0800

    学习中心视频和音频

---
+++ src/main/webapp/js/learnCen/audiocourse.js
@@ -0,0 +1,115 @@
+/**
+ * Author: MengyuWu
+ * Date: 2018/12/7
+ * Description:配置
+ */
+var vm = new Vue({
+    el: '#app',
+    data: {
+        videoData: [],//视频列表
+        navData: [],//导航
+        formInline: { // 搜索表单
+            value: '',
+            name: '',
+            status: "",
+            currPage: 1,
+            pageSize: 10,
+            totalCount:0
+        },
+        tableData: [],//表格数据
+        visible: false,
+        stuMedia: {
+            id:"",
+            stuType: "1",
+            stuTitle: "",
+            comContent: "",
+            deptIds: "",
+            userIds: "",
+            stuDescribe:"",
+            userName:"",//适用人员姓名
+            deptName:"",//适用部门姓名
+        },
+        treeData: [],//法律知识库分类树
+        defaultProps: { // el-tree
+            children: 'list',
+            label: 'classifyName'
+        },
+    },
+    created: function () {
+
+        this.$nextTick(function () {
+
+            //法律分类树数据
+            $.ajax({
+                type: "POST",
+                url: baseURL + "law/tree",
+                contentType: "application/json",
+                success: function(result){
+                    if(result.code === 0){
+                        vm.treeData = result.classifyList;
+                    }else{
+                        alert(result.msg);
+                    }
+                }
+            });
+
+            //获取信息
+            $.ajax({
+                type: "POST",
+                url: baseURL + "stumedia/list",
+                contentType: "application/json",
+                success: function(result){
+                    if(result.code === 0){
+                        vm.videoData = result.page.list;
+                    }else{
+                        alert(result.msg);
+                    }
+                }
+            });
+        })
+        this.$nextTick(function () {
+            this.reload();
+        })
+
+    },
+    methods: {
+        // 查询
+        onSubmit: function () {
+            this.reload();
+        },
+        handleSizeChange: function (val) {
+            this.formInline.pageSize=val;
+            this.reload();
+        },
+        handleCurrentChange: function (val) {
+            this.formInline.currPage=val;
+            this.reload();
+        },
+        // 表单重置
+        resetForm: function (formName) {
+            this.$refs[formName].resetFields();
+        },
+        reload: function () {
+            $.ajax({
+                type: "POST",
+                url: baseURL + "stumedia/list",
+                dataType: "json",
+                data: vm.formInline,
+                success: function (result) {
+                    if (result.code == 0) {
+                        vm.tableData = result.page.list;
+                        vm.formInline.currPage = result.page.currPage;
+                        vm.formInline.pageSize = result.page.pageSize;
+                        vm.formInline.totalCount = parseInt(result.page.totalCount);
+                    } else {
+                        alert(result.msg);
+                    }
+                }
+            });
+        },
+        // el-tree节点点击事件
+        handleNodeClick: function (data) {
+            console.log(data);
+        },
+    }
+});
