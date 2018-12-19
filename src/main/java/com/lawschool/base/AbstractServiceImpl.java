35eb3b69d681716cce6cdc0bd2fa5e23c96bb81e
WangTongYe <xiaoer.wang@qq.com>
2018-12-19 20:02:51 +0800

    基类去掉多余导入

--- src/main/java/com/lawschool/base/AbstractServiceImpl.java
+++ src/main/java/com/lawschool/base/AbstractServiceImpl.java
@@ -1,6 +1,5 @@
 package com.lawschool.base;
 
-import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.baomidou.mybatisplus.toolkit.IdWorker;
 import com.lawschool.beans.User;
