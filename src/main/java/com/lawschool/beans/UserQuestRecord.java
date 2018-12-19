28a1ce93ec7455e77fe4c02ea94323f8eccc90dc
sunxiaokang@sunxiaokang <123456>
2018-12-19 17:35:51 +0800

    做完题目备份

--- src/main/java/com/lawschool/beans/UserQuestRecord.java
+++ src/main/java/com/lawschool/beans/UserQuestRecord.java
@@ -9,14 +9,33 @@ import java.util.Date;
 public class UserQuestRecord {
     @TableId
     private String id;
-
+//用户id
     private String userId;
-
+//题目id
     private String questId;
-
+//操作时间
     private Date opttime;
-
+//我的答案
     private String myAswerId;
+
+
+
+   //正确的的答案
+    private String rightAnswerId;
+
+    //难度
+    private String  questionDifficulty;
+    //题目类型
+    private String   questionType;
+    //知识点
+    private String  specialKnowledgeId;
+
+
+    //来源     闯关禁赛：Checkpoint   /     谁用到新的来源  更新下备注
+    private String source;
+
+
+
 
     public String getId() {
         return id;
