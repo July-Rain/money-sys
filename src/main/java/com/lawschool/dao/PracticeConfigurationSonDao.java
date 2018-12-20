9f636c592b850260dc6f992a693aa93f96f544ee
xuxiang <2434844600@qq.com>
2018-12-20 15:05:41 +0800

    ''

--- src/main/java/com/lawschool/dao/PracticeConfigurationSonDao.java
+++
@@ -1,8 +0,0 @@
-package com.lawschool.dao;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.lawschool.beans.PracticeConfiguration02;
-
-public interface PracticeConfigurationSonDao extends BaseMapper<PracticeConfiguration02> {
-    void insertConfig(PracticeConfiguration02 practiceConfiguration02);
-}
--- src/main/java/com/lawschool/service/PracticeConfigurationSonService.java
+++
@@ -1,8 +0,0 @@
-package com.lawschool.service;
-
-import com.baomidou.mybatisplus.service.IService;
-import com.lawschool.beans.PracticeConfiguration02;
-
-public interface PracticeConfigurationSonService extends IService<PracticeConfiguration02>{
-    void insertConfig(PracticeConfiguration02 practiceConfiguration02);
-}
