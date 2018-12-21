b0c5de505c54a807ee466c86f1d8869cc29a2955
bunan <bunan@sinorock.net>
2018-12-21 17:59:35 +0800

    试题管理部分功能

--- src/main/webapp/js/manageCen/examManage.js
+++ src/main/webapp/js/manageCen/examManage.js
@@ -16,7 +16,7 @@ var vm = new Vue({
             optUser: '',
             stuOptdepartment: '',
             pageNo: 1,
-            limit:10,
+            limit: 10,
             count: 0
         },
         formLabelWidth: '120px',
@@ -29,9 +29,9 @@ var vm = new Vue({
     mounted: function () {
 
     },
-    methods:{
-        layFn(){
-            $(".el-dialog").css("height","auto")
+    methods: {
+        layFn() {
+            $(".el-dialog").css("height", "auto")
         },
         // 文件上传
         handleRemove(file, fileList) {
@@ -44,7 +44,7 @@ var vm = new Vue({
             this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
         },
         beforeRemove(file, fileList) {
-            return this.$confirm(`确定移除 ${ file.name }？`);
+            return this.$confirm(`确定移除 ${file.name}？`);
         },
 
         handleSizeChange: function (val) {
@@ -58,20 +58,50 @@ var vm = new Vue({
             vm.dialogFormVisible = true
         },
         batchImport: function () {
-            
+
         },
         downloadTemp: function () {
-            
-        },
-        handleWatch: function () {
 
+        },
+        handleWatch: function (index, row) {
+            console.table({
+                "row": row
+            })
         },
         handleEdit: function () {
 
         },
-        handleDel: function () {
 
+        handleDel: function (index, row) {
+            this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(function () {
+                $.ajax({
+                    type: "POST",
+                    url: baseURL + "testQuestion/delete",
+                    dataType: "json",
+                    data: {
+                        idList: [row.id]
+                    },
+                    success: function (result) {
+                        if (result.code === 0) {
+                            vm.reload();
+                        } else {
+                            alert(result.msg);
+                        }
+                    }
+                });
+
+            }).catch(function () {
+                vm.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });
+            });
         },
+
         save: function () {
             $.ajax({
                 type: "POST",
@@ -115,13 +145,13 @@ var vm = new Vue({
             });
         }
     },
-    created: function(){
+    created: function () {
         this.$nextTick(function () {
             $.ajax({
                 type: "GET",
                 url: baseURL + "testQuestion/list",
                 contentType: "application/json",
-                data:vm.form,
+                data: vm.form,
                 success: function (result) {
                     if (result.code === 0) {
                         vm.tableData = result.page.list;
