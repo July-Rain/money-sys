f2d07212110242c85e9bd12985e9d09902e94a2a
WangTongYe <xiaoer.wang@qq.com>
2018-12-25 16:48:01 +0800

    勋章相关修改

--- src/main/java/com/lawschool/service/impl/medal/MedalServiceImpl.java
+++ src/main/java/com/lawschool/service/impl/medal/MedalServiceImpl.java
@@ -1,9 +1,9 @@
-package com.lawschool.service.impl;
+package com.lawschool.service.impl.medal;
 
 import com.lawschool.base.AbstractServiceImpl;
-import com.lawschool.beans.MedalEntity;
-import com.lawschool.dao.MedalDao;
-import com.lawschool.service.MedalService;
+import com.lawschool.beans.medal.MedalEntity;
+import com.lawschool.dao.medal.MedalDao;
+import com.lawschool.service.medal.MedalService;
 import org.springframework.stereotype.Service;
 
 @Service
