d4fe8411287eb5a24be72c72e0fbc98313ade132
WangTongYe <xiaoer.wang@qq.com>
2018-12-25 20:13:56 +0800

    我的勋章相关修改

---
+++ src/main/java/com/lawschool/service/impl/medal/UserMedalServiceImpl.java
@@ -0,0 +1,22 @@
+package com.lawschool.service.impl.medal;
+
+import com.lawschool.base.AbstractServiceImpl;
+import com.lawschool.beans.medal.UserMedalEntity;
+import com.lawschool.dao.medal.UserMedalDao;
+import com.lawschool.service.medal.UserMedalService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserMedalServiceImpl extends AbstractServiceImpl<UserMedalDao, UserMedalEntity> implements UserMedalService {
+
+    public List<String> findMedalIdList(String userId) {
+        return dao.findMedalIdList(userId);
+    }
+
+    public Boolean checkUserMedal(UserMedalEntity userMedal){
+        int num = dao.checkUserMedal(userMedal);
+        return (num > 0);
+    }
+}
