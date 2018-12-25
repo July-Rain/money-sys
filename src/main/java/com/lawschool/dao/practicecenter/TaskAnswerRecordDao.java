8dbc87db805fea21951fa4802e601547fa17a3f6
xuxiang <2434844600@qq.com>
2018-12-25 18:42:01 +0800

    组卷练习

---
+++ src/main/java/com/lawschool/dao/practicecenter/TaskAnswerRecordDao.java
@@ -0,0 +1,13 @@
+package com.lawschool.dao.practicecenter;
+
+import com.lawschool.base.AbstractDao;
+import com.lawschool.beans.practicecenter.TaskAnswerRecordEntity;
+
+/**
+ * @Auther: Moon
+ * @Date: 2018/12/21 17:57
+ * @Description:
+ */
+public interface TaskAnswerRecordDao extends AbstractDao<TaskAnswerRecordEntity> {
+
+}
