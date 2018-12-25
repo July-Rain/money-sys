953fca971fb153721258091ac06eeb159d842cec
xuxiang <2434844600@qq.com>
2018-12-25 18:42:31 +0800

    练习配置Dao

---
+++ src/main/java/com/lawschool/dao/ExerciseConditionDao.java
@@ -0,0 +1,13 @@
+package com.lawschool.dao;
+
+import com.lawschool.base.AbstractDao;
+import com.lawschool.beans.ExerciseConditionEntity;
+
+/**
+ * @Auther: Moon
+ * @Date: 2018/12/20 15:41
+ * @Description:
+ */
+public interface ExerciseConditionDao extends AbstractDao<ExerciseConditionEntity> {
+
+}
