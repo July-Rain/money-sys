4c30a38c677581afdb17d4ac8112665dc15d0876
xuxiang <2434844600@qq.com>
2018-12-28 19:31:28 +0800

    组卷任务

--- src/main/java/com/lawschool/service/practicecenter/TaskExerciseService.java
+++ src/main/java/com/lawschool/service/practicecenter/TaskExerciseService.java
@@ -5,8 +5,11 @@ import com.lawschool.base.Page;
 import com.lawschool.beans.ExerciseConfigureEntity;
 import com.lawschool.beans.practicecenter.TaskExerciseEntity;
 import com.lawschool.form.CommonForm;
+import com.lawschool.form.QuestForm;
+import com.lawschool.form.ThemeAnswerForm;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: Moon
@@ -23,4 +26,31 @@ public interface TaskExerciseService extends AbstractService<TaskExerciseEntity>
      */
     List<CommonForm> findByUserAndConIds(String userId, List<String> list);
 
+    /**
+     * 组卷练习展示试卷信息
+     * @param configureId 练习配置ID
+     * @param id 个人练习ID
+     * @param userId 用户ID
+     * @param isNew 是否需要创建个人练习任务
+     * @param limit 每页显示条数
+     * @param page 当前页
+     * @return 包含2个结果，一个题目list，一个总页数
+     */
+    Map<String, Object> showPaper(String configureId, String id,
+                                  String userId, Boolean isNew,
+                                  Integer limit, Integer page);
+
+    /**
+     * 组卷练习保存答题情况
+     * @param list
+     */
+    void preserve(List<ThemeAnswerForm> list, String userId);
+
+    /**
+     * 更新任务状态
+     * @param id
+     * @param status
+     * @return
+     */
+    boolean updateStatus(String id, Integer status);
 }
