c83decea92963d3e669ced55097132a37d803a7e
bunan <bunan@sinorock.net>
2019-01-04 11:44:21 +0800

    学校社区页面

--- src/main/webapp/js/school/index.js
+++ src/main/webapp/js/school/index.js
@@ -8,9 +8,17 @@ var vm = new Vue({
     data: {
         form:{
             desc: ""
-        }
+        },
+        menuIndex: 0,
+        navList: ["全部","试题","试题报错","学习","案例","建议","常见问题解答","我的参与"]
     },
     created: function (){
 
+    },
+    methods:{
+        refreshTab: function (index) {
+            this.menuIndex = index;
+            // $ajax 更新数据
+        }
     }
 });
