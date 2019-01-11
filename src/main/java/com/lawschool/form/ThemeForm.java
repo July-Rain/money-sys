19632ebf25829969050031e7eae18616dfd53ad2
xuxiang <2434844600@qq.com>
2019-01-11 18:01:58 +0800

    form增加字段

--- src/main/java/com/lawschool/form/ThemeForm.java
+++ src/main/java/com/lawschool/form/ThemeForm.java
@@ -11,6 +11,7 @@ public class ThemeForm {
     private String userId;// 用户ID
     private List<ThemeAnswerForm> list;
     private String typeId;// 主题类型ID
+    private String typeName;// 主题名称
 
     public String getId() {
         return id;
@@ -50,5 +51,13 @@ public class ThemeForm {
 
     public void setTypeId(String typeId) {
         this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
     }
 }
