8834e435bd06e596d26c08d201f7b478edff338b
bunan <bunan@sinorock.net>
2019-01-18 18:02:33 +0800

    coding

--- src/main/webapp/js/competition/competeCenter.js
+++ src/main/webapp/js/competition/competeCenter.js
@@ -48,7 +48,8 @@ var vm = new Vue({
                 }
             });
 
-
+            var _html = document.getElementById('qiaojie').innerHTML;
+            vm.getTempName(_html);
         })
 
     },
@@ -56,8 +57,46 @@ var vm = new Vue({
         handleClick : function(tab, event){
             console.log(tab, event);
         },
+        getTempName: function (_html) {
 
+
+
+
+            let a = ["中文姓名","证件类型"];
+            let b = [{"name":"证件类型","value":"IDTYPE"},{"name":"中文姓名","value":"CHINESE_NAME"},{"name":"证件号码","value":"IDNO"}];
+            let result = []
+            a.map((i)=>{
+                b.map((j)=>{
+                    if(i === j.name){
+                        result.push(j.value)
+                    }
+                })
+            });
+            console.log(result);
+
+
+
+            console.info("_html",_html)
+            console.log(typeof _html)
+            //
+            // for(var i = 0 ; i< list.length ; i ++ ){
+            //     var s = $(list[i]).text();//获取里面的博客摘要内容
+            //     var dd=s.replace(/<[^>]+>/g,"");//截取html标签
+            //     var dds=dd.replace(/&nbsp;/ig,"");//截取空格等特殊标签
+            //
+            //     $(list[i]).text("");//清空里面内容
+            //     $(list[i]).text(dds.substring(0,200) + "...")//截取200个字符
+            // }
+
+
+
+
+
+
+
+        }
 
 
     }
+
 });
