079d4725e62e991d969388bce93485ab55d77b3d
xuxiang <2434844600@qq.com>
2019-01-28 15:26:17 +0800

    投稿dao

--- src/main/java/com/lawschool/dao/ManuscriptDao.java
+++ src/main/java/com/lawschool/dao/ManuscriptDao.java
@@ -2,6 +2,8 @@ package com.lawschool.dao;
 
 import com.lawschool.base.AbstractDao;
 import com.lawschool.beans.ManuscriptEntity;
+import org.apache.ibatis.annotations.Param;
 
 public interface ManuscriptDao extends AbstractDao<ManuscriptEntity> {
+    Boolean updateExamine(@Param("id") String id, @Param("status") Integer status);
 }
